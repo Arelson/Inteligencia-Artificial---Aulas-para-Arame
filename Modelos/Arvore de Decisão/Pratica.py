d3ff6eae0b7a87af2186a8cfbe96961e433be182
Arlison <arlisong14@gmail.com>
2026-03-22 18:39:24 -0300

    plotando a arvore de decisão

--- Modelos/Arvore de Decisão/Pratica.py
+++ Modelos/Arvore de Decisão/Pratica.py
@@ -1,15 +1,20 @@
 from sklearn import tree
+from matplotlib import pyplot as plt
 
 # Separando açai do cupuaçu em que [peso, cor: 1(roxo), 2(branco)]
 
 caracteristica = [[15, 1], [30, 1], [200, 2],[350, 2]]
-resposta = ['Açai', 'Açai', 'Cupuaçu', 'Cupaçu']
+resposta = ['Açai', 'Açai', 'Cupuaçu', 'Cupuaçu']
 
 # Criando a arvore
 arvore = tree.DecisionTreeClassifier()
 
 # Treinando a arvore
 arvore.fit(caracteristica, resposta)
+
+plt.figure(figsize=(10, 6))
+tree.plot_tree(arvore, feature_names= ['Peso', 'Cor'], class_names= resposta, filled= True)
+plt.show()
 
 # Colocando caracteristicas da fruta desconhecida
 nova_fruta = [[234, 2]]
